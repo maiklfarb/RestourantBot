4110761acd057f981d314a273907fbe1b95b9070
maiklfarb <MishaFarb09@gmail.com>
2023-04-20 23:01:47 +0300

    1 - код ревью

--- DataContext.py
+++ DataContext.py
@@ -1,27 +1,26 @@
 from peewee import *
-db = SqliteDatabase('data.sqlite')
+db = SqliteDatabase('data/data.sqlite')
 
 class BaseModel(Model):
     class Meta:
         database = db
 
 class Role(BaseModel):
-    id = PrimaryKeyField(null=False, column_name="roleId")
-    name = TextField(null=False, column_name="name")
+    id = PrimaryKeyField(null=False, column_name="RoleID")
+    name = TextField(null=False, column_name="Name")
     class Meta:
         table_name = "Roles"
 
 class Client(BaseModel):
-    id = PrimaryKeyField(null=False, column_name="clientID")
-    name = TextField(null=False, column_name="name")
-    chatId = IntegerField(null=False, column_name="ChatId")
-    username = TextField(null=False, column_name="username")
-    carId = ForeignKeyField(Role, column_name="roleId", backref="Roles")
+    id = PrimaryKeyField(null=False, column_name="ClientID")
+    name = TextField(null=False, column_name="Name")
+    chatId = IntegerField(null=False, column_name="ChatID")
+    username = TextField(null=False, column_name="Username")
+    carId = ForeignKeyField(Role, column_name="RoleID", backref="Roles")
 
     class Meta:
         table_name = "Clients"
 
-print(__name__)
 if __name__ == "__main__":
     Role.create_table()
     Client.create_table()
